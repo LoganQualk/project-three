73286e9eb144c4465a355d528b9e912ad8a5d5de
LoganQualk <loganq@gmail.com>
2021-07-30 19:04:54 -0400

    basically working, small quality of life changes could be made

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,22 @@ var parentUl = document.querySelector("#parent-ul")
 var signUpButton = document.querySelector("#sign-up");
 var selectUser = document.querySelector("#select-user");
 let myStorage = window.localStorage;
-let todos = [];
+let todos = JSON.parse(myStorage.getItem("todos"));
 let i = 0;
+
+window.onload = function () {
+    for (let i = 0; i < todos.length; i++) {
+        let userid = todos[i].userid;
+        if (!checkIfUserNameExists(userid)) {
+            selectUser.options[selectUser.options.length] = new Option(userid, userid);
+        }
+        // let isComplete = todos[i].complete;
+        // if (isComplete) {
+        //     markAsComplete();
+        // }
+    }
+    showToDos();
+}
 // let j = 0; // user id
 
 // function submitToDo() {
@@ -49,21 +63,20 @@ function createListItem() {
     let li = document.createElement("li");
     let p = document.createElement("p");
     let butt = document.createElement("button");
-    p = todos[i].toDo;
+    p = temp.toDo;
     butt.innerText = "Mark as complete!";
     butt.setAttribute("class", "mark-complete-button");
-    li.append("To Do: ", p, butt);
+    li.append("To Do: ", p, butt, "User: ", userName.value);
     li.dataset.id = i;
     parentUl.appendChild(li);
     parentUl.insertBefore(li, parentUl.firstChild);
     butt.addEventListener("click", markAsComplete);
     butt.style.marginLeft = "1rem";
+    butt.style.marginRight = "1rem";
     if (!checkIfUserNameExists(userName.value)) {
         selectUser.options[selectUser.options.length] = new Option(userName.value, userName.value);
     }
     myStorage.setItem("todos", JSON.stringify(todos));
-    showToDos();
-    console.log(myStorage);
     i++;
 }
 
@@ -74,15 +87,11 @@ function checkIfUserNameExists(userName) {
 
 function markAsComplete() {
     this.parentElement.style.textDecoration = "line-through";
-    console.log(this.parentElement.dataset.id);
-    console.log(todos[this.parentElement.dataset.id].complete);
     todos[this.parentElement.dataset.id].complete = true;
-    console.log(todos[this.parentElement.dataset.id].complete);
     // this.parentElement.remove();
 }
 
 function updateList() {
-    console.log(todos.filter((todo) => todo.userid === selectUser.value || selectUser.value === ""));
     while (parentUl.firstChild) {
         parentUl.removeChild(parentUl.lastChild);
     }
@@ -96,19 +105,21 @@ function updateList() {
             p = todos[i].toDo;
             butt.innerText = "Mark as complete!";
             butt.setAttribute("class", "mark-complete-button");
-            li.append("To Do: ", p, butt, underUser);
+            li.append("To Do: ", p, butt, "User: ", underUser);
             li.dataset.id = i;
             parentUl.appendChild(li);
             parentUl.insertBefore(li, parentUl.firstChild);
             butt.addEventListener("click", markAsComplete);
             butt.style.marginLeft = "1rem";
+            butt.style.marginRight = "1rem";
         }
     }
 }
 
 function showToDos() {
     let todos = myStorage.getItem("todos");
-    if (todos) {
+    todos = JSON.parse(todos);
+    for (let i = 0; i < todos.length; i++) {
         let li = document.createElement("li");
         let p = document.createElement("p");
         let butt = document.createElement("button");
@@ -117,22 +128,29 @@ function showToDos() {
         p = todos[i].toDo;
         butt.innerText = "Mark as complete!";
         butt.setAttribute("class", "mark-complete-button");
-        li.append("To Do: ", p, butt, underUser);
+        li.append("To Do: ", p, butt, "User: ", underUser);
         li.dataset.id = i;
         parentUl.appendChild(li);
         parentUl.insertBefore(li, parentUl.firstChild);
         butt.addEventListener("click", markAsComplete);
         butt.style.marginLeft = "1rem";
+        butt.style.marginRight = "1rem";
     }
 }
 
 selectUser.addEventListener("change", updateList);
-
 submitButton.addEventListener("click", createListItem);
 input.addEventListener("keyup", function(e) {
     // https://www.tutorialspoint.com/javascript-submit-textbox-on-pressing-enter
     if (e.code === 'Enter') {
         e.preventDefault();
-        submitToDo();
+        createListItem();
+    }
+}, false);
+userName.addEventListener("keyup", function(e) {
+    // https://www.tutorialspoint.com/javascript-submit-textbox-on-pressing-enter
+    if (e.code === 'Enter') {
+        e.preventDefault();
+        createListItem();
     }
 }, false);
